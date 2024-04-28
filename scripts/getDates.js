437d4d0bde68b5e7ce7c096f67156d235fdd0931
harifenitra jonathan rajaonarison <harifenitra98nathan@gmail.com>
2024-04-28 16:03:05 +0300

    W01 Activity

---
+++ scripts/getDates.js
@@ -0,0 +1,7 @@
+
+const currentYear = new Date().getFullYear();
+const copyrightText = document.getElementById('copyright');
+copyrightText.textContent = `\u00A9 ${currentYear} Harifenitra Jonathan, Madagascar, Antsirabe`;
+
+const lastModifiedText = document.getElementById('lastModified');
+lastModifiedText.textContent = `Last modified: ${document.lastModified}`;
