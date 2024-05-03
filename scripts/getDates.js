c3881777df9edb75e1c9aa8a683fe677fe6d1f5d
harifenitra jonathan rajaonarison <harifenitra98nathan@gmail.com>
2024-05-03 17:32:12 +0300

    home page enhancement

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -5,3 +5,21 @@ copyrightText.textContent = `\u00A9 ${currentYear} Harifenitra Jonathan, Madagas
 
 const lastModifiedText = document.getElementById('lastModified');
 lastModifiedText.textContent = `Last modified: ${document.lastModified}`;
+
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('nav ul');
+
+hamburger.addEventListener('click', () => {
+    navMenu.classList.toggle('show');
+});
+
+navMenu.addEventListener('click', () => {
+    navMenu.classList.remove('show');
+});
+
+const darkModeToggle = document.querySelector('.dark-mode-toggle');
+const mainArea = document.querySelector('main');
+
+darkModeToggle.addEventListener('click', () => {
+    mainArea.classList.toggle('dark-mode');
+});
