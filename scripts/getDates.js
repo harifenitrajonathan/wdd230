157a52a8315896ffccc9ae421f5194ef76eacaa4
harifenitra jonathan rajaonarison <harifenitra98nathan@gmail.com>
2024-05-11 15:49:04 +0300

    Team activity done

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -5,6 +5,13 @@ copyrightText.textContent = `\u00A9 ${currentYear} Harifenitra Jonathan, Madagas
 const lastModifiedText = document.getElementById('lastModified');
 lastModifiedText.textContent = `Last modified: ${document.lastModified}`;
 
+const darkModeToggle = document.querySelector('.dark-mode-toggle');
+const mainArea = document.querySelector('main');
+
+darkModeToggle.addEventListener('click', () => {
+    mainArea.classList.toggle('dark-mode');
+});
+
 const hamburger = document.querySelector('.hamburger');
 const navMenu = document.querySelector('nav ul');
 
@@ -15,12 +22,34 @@ hamburger.addEventListener('click', () => {
 
 navMenu.addEventListener('click', () => {
     navMenu.classList.remove('show');
-    hamburger.textContent = '☰'; // Reset the hamburger symbol to ☰ when the nav menu is closed
+    hamburger.textContent = '☰';
 });
 
-const darkModeToggle = document.querySelector('.dark-mode-toggle');
-const mainArea = document.querySelector('main');
-
-darkModeToggle.addEventListener('click', () => {
-    mainArea.classList.toggle('dark-mode');
+// Add an event listener to close the nav menu if clicked outside
+document.addEventListener('click', (event) => {
+    const isClickInsideNav = navMenu.contains(event.target);
+    const isClickOnHamburger = hamburger.contains(event.target);
+    if (!isClickInsideNav && !isClickOnHamburger) {
+        navMenu.classList.remove('show');
+        hamburger.textContent = '☰';
+    }
 });
+
+function updateVisitCount() {
+    let count = localStorage.getItem('visitCount');
+    count = count ? parseInt(count) + 1 : 1;
+    document.getElementById('visit-count').textContent = count;
+    localStorage.setItem('visitCount', count.toString());
+}
+
+function initializeVisitCount() {
+    let count = localStorage.getItem('visitCount');
+
+    if (count) {
+        document.getElementById('visit-count').textContent = count;
+    }
+}
+
+document.addEventListener('DOMContentLoaded', initializeVisitCount);
+
+updateVisitCount();
